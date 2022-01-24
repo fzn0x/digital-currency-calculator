7a510c69b6b8772ea964730abe39ba93cb097873
FNC <fncolon@pm.me>
2022-01-24 14:22:43 +0700

    feat: init digital currency calculator

---
+++ api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const fetch = require("node-fetch");
+const app = express();
+
+app.get("/api", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
+  res.end(`
+  ETH currency exchange rates: <a href="/api/eth">ETH here</a>
+  `);
+});
+
+app.get("/api/:currency", async (req, res) => {
+  const response = await fetch(
+    `https://api.coinbase.com/v2/exchange-rates?currency=${req.params.currency}`
+  );
+  let json = await response.json();
+
+  if (req.query.to) {
+    if (req.query.amount) {
+      const rate = json.data.rates[req.query.to.toUpperCase()];
+      const price = (Number(req.query.amount) || 0) / rate;
+      return res.json({
+        currency: json.data.currency,
+        rate: rate,
+        price: price,
+        amount: price * rate,
+      });
+    } else {
+      return res.json({
+        currency: json.data.currency,
+        rate: json.data.rates[req.query.to.toUpperCase()],
+      });
+    }
+  } else {
+    return res.json(json.data);
+  }
+});
+
+module.exports = app;
